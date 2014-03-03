a09526698afc770893f28f9d27bb77afab309b87
Ryu, Han-seok <acoustic80@nate.com>
2014-03-03 19:04:38 +0900

    init

---
+++ tsetpyglet.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+#lesson4.py
+
+# See original source and C based tutorial at http://nehe.gamedev.net
+#This code was created by Richard Campbell '99
+
+#(ported to Python/PyOpenGL by John Ferguson 2000)
+#John Ferguson at hakuin@voicenet.com
+
+#Code ported for use with pyglet by Jess Hill (Jestermon) 2009
+#jestermon.weebly.com
+#jestermonster@gmail.com
+
+#because these lessons sometimes need  openGL GLUT, you need to install
+#pyonlgl as well as pyglet, in order for this sample them to work
+#pyopengl ~ http://pyopengl.sourceforge.net
+#pyglet   ~ http://www.pyglet.org
+
+import pyglet
+from pyglet.gl import *
+from pyglet.window import key
+from OpenGL.GLUT import * #<<<==Needed for GLUT calls
+from objloader import *
+
+##################################World
+class World(pyglet.window.Window):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    objfile = 'resource/complex2.obj' 
+    obj = OBJ(objfile)
+    def __init__(self):
+        config = Config(sample_buffers=1, samples=4,
+                    depth_size=16, double_buffer=True,)
+        try:
+            super(World, self).__init__(resizable=True, config=config)
+        except:
+            super(World, self).__init__(resizable=True)
+        self.setup()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def setup(self):
+        self.width = 640
+        self.height = 480
+        self.rtri = 0.0    #(was global)
+        self.rquad = 0.0   #(was global)
+        self.InitGL(self.width, self.height)
+        pyglet.clock.schedule_interval(self.update, 1/60.0) # update at 60Hz
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def update(self,dt):
+        self.DrawGLScene()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def on_draw(self):
+        self.DrawGLScene()
+        glCallList(self.obj.gl_list)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def on_resize(self,w,h):
+        self.ReSizeGLScene(w,h)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # A general OpenGL initialization function.  Sets all of the initial parameters.
+    def InitGL(self,Width, Height):				# We call this right after our OpenGL window is created.
+        glClearColor(0.0, 0.0, 0.0, 0.0)	   # This Will Clear The Background Color To Black
+        glClearDepth(1.0)					      # Enables Clearing Of The Depth Buffer
+        glDepthFunc(GL_LESS)				      # The Type Of Depth Test To Do
+        glEnable(GL_DEPTH_TEST)			    	# Enables Depth Testing
+        glShadeModel(GL_SMOOTH)			   	# Enables Smooth Color Shading
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()					      # Reset The Projection Matrix
+   	 									            # Calculate The Aspect Ratio Of The Window
+        #(pyglet initializes the screen so we ignore this call)
+        #gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
+    def ReSizeGLScene(self,Width, Height):
+        if Height == 0:						      # Prevent A Divide By Zero If The Window Is Too Small
+     	      Height = 1
+        glViewport(0, 0, Width, Height)		# Reset The Current Viewport And Perspective Transformation
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # The main drawing function.
+    def DrawGLScene(self):
+        global rtri, rquad
+
+        # Clear The Screen And The Depth Buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()					# Reset The View
+
+        # Move Left 1.5 units and into the screen 6.0 units.
+        glTranslatef(-1.5, 0.0, -6.0)
+
+        # We have smooth color mode on, this will blend across the vertices.
+        # Draw a triangle rotated on the Y axis.
+        # glRotatef(self.rtri, 0.0, 0.0, -1.0)      # Rotate
+        glBegin(GL_POLYGON)                 # Start drawing a polygon
+        glColor3f(1.0, 0.0, 0.0)            # Red
+        glVertex3f(0.0, 1.0, 0.0)           # Top
+        glColor3f(0.0, 1.0, 0.0)            # Green
+        glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
+        glColor3f(0.0, 0.0, 1.0)            # Blue
+        glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
+        glEnd()                             # We are done with the polygon
+
+        # We are "undoing" the rotation so that we may rotate the quad on its own axis.
+        # We also "undo" the prior translate.  
+        # This could also have been done using the matrix stack.
+        glLoadIdentity()
+
+        # Move Right 1.5 units and into the screen 6.0 units.
+        glTranslatef(1.5, 0.0, -6.0)
+
+        # Draw a square (quadrilateral) rotated on the X axis.
+        glRotatef(self.rquad, 0.0, 0.0, -1.0)		# Rotate
+        glColor3f(0.3, 0.5, 1.0)            # Bluish shade
+        glBegin(GL_QUADS)                   # Start drawing a 4 sided polygon
+        glVertex3f(-1.0, 1.0, 0.0)          # Top Left
+        glVertex3f(1.0, 1.0, 0.0)           # Top Right
+        glVertex3f(1.0, -1.0, 0.0)          # Bottom Right
+        glVertex3f(-1.0, -1.0, 0.0)         # Bottom Left
+        glEnd()                             # We are done with the polygon
+
+        # What values to use?  Well, if you have a FAST machine and a FAST 3D Card, then
+        # large values make an unpleasant display with flickering and tearing.  I found that
+        # smaller values work better, but this was based on my experience.
+        #(2009.. 9 years after this code was written, this still applies.. unless you use)
+        #(a timed display, as done here with pyglet.clock.schedule_interval(self.update, 1/60.0) #updates at 60Hz)
+        # self.rtri  = self.rtri + 1.0                  # Increase The Rotation Variable For The Triangle
+        self.rquad = self.rquad + 1.0                 # Decrease The Rotation Variable For The Quad
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.ESCAPE:
+            self.dispatch_event('on_close')
+        #  since this is double buffered, swap the buffers to display what just got drawn.
+        #(pyglet provides the swap, so we dont use the swap here)
+        #glutSwapBuffers()
+
+##################################main
+if __name__ == "__main__":
+    window = World()
+    # glutFullScreen()
+    pyglet.app.run()
+
+
+
+
+
